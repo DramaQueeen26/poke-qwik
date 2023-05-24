25db6770f78fbf218f3099100f68f9a6a14d66e2
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-05-24 19:52:11 -0400

    HTTP request and adding typing

--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,9 +1,42 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { Link, routeLoader$ } from '@builder.io/qwik-city';
+import type { BasicPokemonInfo, PokemonListResponse } from '~/interfaces';
+
+export const usePokemonList = routeLoader$<BasicPokemonInfo[]>( async() => {
+
+  const resp = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=10');
+  const data = await resp.json() as PokemonListResponse;
+
+  return data.results;
+
+});
 
 export default component$(() => {
+
+  const pokemons = usePokemonList();
+
   return (
-    <>Hola mundo</>
+    <>  
+      <div class="flex flex-col">
+        <span class="my-5 text-5xl">Status</span>
+        <span class="mt-1">Current offset:</span>
+        <span class="mt-1">Loading page: </span>  
+      </div>
+
+      <div class="mt-10">
+        <Link class="btn btn-primary mr-2">Previous</Link>
+        <Link class="btn btn-primary mr-2">Next</Link>
+      </div>
+
+      <div class="grid grid-cols-6 mt-5">
+        { pokemons.value.map( ({ name }) => (
+          <div key={ name } class="m-5 flex flex-col justify-center">
+            <span class="capitalize">{ name }</span>
+          </div>
+        ))}
+      </div>
+    </>
   );
 });
 
