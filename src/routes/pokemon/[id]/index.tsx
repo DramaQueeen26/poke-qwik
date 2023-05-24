9034c014f9cf8ab8e67bd6a823da36531ba63921
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-05-24 18:55:02 -0400

    routeLoader and validating

--- src/routes/pokemon/[id]/index.tsx
+++ src/routes/pokemon/[id]/index.tsx
@@ -1,16 +1,32 @@
 import { component$ } from '@builder.io/qwik';
-import { useLocation, type DocumentHead} from '@builder.io/qwik-city';
+import { type DocumentHead, routeLoader$} from '@builder.io/qwik-city';
 import { PokemonImage } from '../../../components/pokemons/pokemon-image';
+
+// ? El routeLoader se ejecuta antes de renderizar el componente
+export const usePokemonId = routeLoader$<number>( ({ params, redirect }) => {
+
+  const id = Number(params.id);
+
+  if( isNaN( id ) ) redirect(301, '/');
+  if( id <= 0 || id >= 1000 ) redirect(301, '/');
+
+  return id;
+
+}); 
 
 export default component$(() => {
 
-  const location = useLocation();
-  const id = location.params.id;
+  // * Con useLocation
+  // const location = useLocation();
+  // const id = location.params.id; 
+
+  // * Con routeLoader
+  const id = usePokemonId();
 
   return (
     <>
         <span class="text-5xl">Pokemon: { id }</span>
-        <PokemonImage id={ id } isVisible={ true } />
+        <PokemonImage id={ id.value } isVisible={ true } />
     </>
   );
 });
