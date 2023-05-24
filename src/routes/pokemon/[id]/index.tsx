4625608484c82728dfa7cef77bb5d0efb6882637
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-05-24 10:52:15 -0400

    Adding Link and page title

--- src/routes/pokemon/[id]/index.tsx
+++ src/routes/pokemon/[id]/index.tsx
@@ -1,4 +1,5 @@
 import { component$ } from '@builder.io/qwik';
+import type { DocumentHead} from '@builder.io/qwik-city';
 import { routeLoader$ } from '@builder.io/qwik-city';
 
 export const useParams = routeLoader$(async ({ params }) => {
@@ -16,3 +17,13 @@ export default component$(() => {
     </>
   );
 });
+
+export const head: DocumentHead = {
+  title: 'Pokemon Info',
+  meta: [
+    {
+      name: 'description',
+      content: 'Qwik Poke App',
+    },
+  ],
+};
--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,7 +1,18 @@
 import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
     <>Hola mundo</>
   );
 });
+
+export const head: DocumentHead = {
+  title: 'SSR List',
+  meta: [
+    {
+      name: 'description',
+      content: 'Qwik Poke App',
+    },
+  ],
+};
